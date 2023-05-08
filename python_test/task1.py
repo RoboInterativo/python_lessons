c646e7b62d9ffcc370539fd3b6bce933dfa3aa61
chiefexb <chief_exb@mail.ru>
2023-05-08 22:58:20 +0300

    fix

---
+++ python_test/task1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a = input('')
+b = input('')
+#print( float(a) * float(b) )
+print (a*b)
---
+++ python_test/task1_test.py
@@ -0,0 +1,37 @@
+import pytest
+import subprocess
+import os
+from pathlib import Path
+
+
+
+def test_a_repl_session():
+    path2 = Path(__file__).parent.absolute()
+    print (path2)
+    comlist = [f'{path2}/task1.py']
+    script = b'1\n2\n\n'
+    res = subprocess.run(comlist, input=script,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print (res)
+    # assert res.returncode == 0
+    assert res.stdout==b'2.0\n'
+    #assert res.stderr
+#
+# scripts = pathlib.Path(__file__, '..', 'scripts').resolve().glob('*.py')
+#
+#
+# @pytest.mark.parametrize('script', scripts)
+# def test_script_execution():
+#     runpy.run_module("task1.py")
+
+
+#
+# def test_myoutput(capsys):  # or use "capfd" for fd-level
+#     print("hello")
+#     sys.stderr.write("world\n")
+#     captured = capsys.readouterr()
+#     assert captured.out == "hello\n"
+#     assert captured.err == "world\n"
+#     print("next")
+#     captured = capsys.readouterr()
+#     assert captured.out == "next\n"

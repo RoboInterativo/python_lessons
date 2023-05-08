6034eeaf12d46ab94cad8f7c8fd230f0e099457e
chiefexb <chief_exb@mail.ru>
2023-05-08 23:06:04 +0300

    123

--- python_test/task1_test.py
+++ python_test/task1_test.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 
-def test_a_repl_session():
+def test_task1():
     path2 = Path(__file__).parent.absolute()
     print (path2)
     comlist = [f'{path2}/task1.py']
@@ -16,6 +16,17 @@ def test_a_repl_session():
     # assert res.returncode == 0
     assert res.stdout==b'2.0\n'
     #assert res.stderr
+
+def test_task2():
+    path2 = Path(__file__).parent.absolute()
+    print (path2)
+    comlist = [f'{path2}/task2.py']
+    script = b'1\n2\n\n'
+    res = subprocess.run(comlist, input=script,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print (res)
+    # assert res.returncode == 0
+    assert res.stdout==b'2.0\n'
 #
 # scripts = pathlib.Path(__file__, '..', 'scripts').resolve().glob('*.py')
 #
--- python_test/task2.py
+++ python_test/task2.py
@@ -2,4 +2,5 @@
 
 a = input('')
 b = input('')
-print( float(a) * float(b) )
+#print( float(a) * float(b) )
+print (a*b)

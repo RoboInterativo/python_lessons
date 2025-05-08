c91c605471a9f7d8b952e2bdcd7dd914958a3a6e
a.shilo <a.shilo@baum.ru>
2025-05-08 15:40:02 +0300

    123

--- summ_array/sum_test.py
+++ summ_array/sum_test.py
@@ -3,7 +3,7 @@ from main import *
 # test_example.py
 import pytest
 
-@pytest.mark.parametrize(l1, l2, expected", [
+@pytest.mark.parametrize("l1, l2, expected", [
     ("4", "1 2 3 4", 10),  # Тест-кейс 1
     ("7", "1000000 826 19 3 999997 1 100000", 2100846),# Тест-кейс 2
 

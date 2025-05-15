f356ac44173ede91aa010203dccc71831558729b
a.shilo <a.shilo@baum.ru>
2025-05-15 18:48:43 +0300

    123

--- anna/task2.py
+++ anna/task2.py
@@ -9,9 +9,9 @@ for i in range(1, len(n) - 1):
     prev = int(n[i - 1])%2==0
     next_ = int(n[i + 1])%2==0
 
-    if (current and prev and next_)==True:
+    if ( current and prev and next_)==True:
         interes=interes+1
-    if (current and prev and next_)==False:
+    if (not current and not prev and not next_)==False:
             interes=interes+1
 
 print(interes)

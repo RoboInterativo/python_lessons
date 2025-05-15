648d765c8edf6591616ec5b7d0f658e215a34541
a.shilo <a.shilo@baum.ru>
2025-05-15 20:19:40 +0300

    123

--- anna/task7.py
+++ anna/task7.py
@@ -1,7 +1,7 @@
 n = input().strip()  # Ввод числа (например, "51224")
 summa=0
 
-for i in range(1, len(n) - 1):
+for i in range(n):
     current = int(n[i])
     if current %2==0:
         summa=summa+current

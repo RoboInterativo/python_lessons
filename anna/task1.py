e70ef67427f1a2bb4e10d032f9f72e6d1ea2e7d4
a.shilo <a.shilo@baum.ru>
2025-05-15 18:21:08 +0300

    123

--- anna/task1.py
+++ anna/task1.py
@@ -1,15 +1,15 @@
-n = input().strip()  # Ввод числа (например, "51224")
-hills = 0  # Счётчик "горок"
-valleys = 0  # Счётчик "ям"
+n = input().strip()
+hills = 0
+valleys = 0
 
 for i in range(1, len(n) - 1):
     current = int(n[i])
     prev = int(n[i - 1])
     next_ = int(n[i + 1])
 
-    if current > prev and current > next_:
+    if current >= prev and current >= next_:  # Нестрогое неравенство
         hills += 1
-    elif current < prev and current < next_:
+    elif current <= prev and current <= next_:  # Нестрогое неравенство
         valleys += 1
 
 print(hills, valleys)

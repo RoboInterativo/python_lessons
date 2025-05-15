07d4ed11a0bce14badb5169dcc0bbb85d5c58b60
a.shilo <a.shilo@baum.ru>
2025-05-15 19:54:55 +0300

    123

---
+++ anna/task5.py
@@ -0,0 +1,24 @@
+#Минимальное 2х значное кратное 5
+l=[]
+for i in range(n):
+    num=int(input())
+    l.append(num)
+
+
+list5=[]
+for num in l:
+    if (num>10 and num<=99):
+        if num %5==0:
+            list5.append(num)
+
+
+
+
+
+min=list5[0]
+
+for num in list5:
+    if num<min:
+        min=num
+
+print(min)

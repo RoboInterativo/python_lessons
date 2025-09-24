db19bc1500c7770486ae43b2f5e47824fa6478b4
a.shilo <a.shilo@baum.ru>
2025-09-24 19:23:35 +0300

    123

--- perf_tasks/task03/task.py
+++ perf_tasks/task03/task.py
@@ -10,8 +10,18 @@ def myfunc(string1):
 import re
 
 # Регулярное выражение, составленное в MATLAB
-pattern = re.compile(r"^\S+@\S+\.\S+$")
 
+phone_pattern = re.compile(r'^(\+7|8)[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$')
+password_pattern = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$')
+name_pattern = re.compile(r'^[A-Za-zА-Яа-яЁё\s\-]+$')
+username_pattern = re.compile(r'^[a-zA-Z0-9_]{3,20}$')
+email_pattern=re.compile(r"^\S+@\S+\.\S+$")
+def validate(valstr,pattern):
 # Проводим проверку электронного адреса
-is_valid = pattern.match("user@example.com")
-print(is_valid is not None)  # Вернёт "True", если адрес действителен, и "False" в противном случае
+    #pattern = re.compile()
+    is_valid= pattern.match(valstr)
+    result=is_valid  is not None
+    return  result
+#print(is_valid is not None)  # Вернёт "True", если адрес действителен, и "False" в противном случае
+rez=validate("1@1.ru",email_pattern)
+print  (rez,type(rez))
--- perf_tasks/task03/task1_test.py
+++ perf_tasks/task03/task1_test.py
@@ -3,11 +3,28 @@ import pytest
 from task import *
 
 
-def test_1():
-    result= myfunc("abba")
+def test_email():
+    result= validate("alex.pricker@gmail.com",email_pattern)
     assert result ==True
 
-
-def test_2():
-    result= myfunc("abbu")
+def test_broke_email():
+    result= validate("alex.prickergmail.com",email_pattern)
     assert result ==False
+
+def test_phone():
+    result= validate("+7-968-194-67-60",phone_pattern)
+    assert result ==True
+
+def test_login():
+    result= validate("alexpricker",username_pattern)
+    assert result ==True
+
+def test_wrong_login():
+    result= validate("123alexpricker",username_pattern)
+    assert result ==False
+# def test_broke_email():
+#     result= validate("alex.prickergmail.com",email_pattern)
+#     assert result ==False
+# def test_2():
+#     result= myfunc("abbu")
+#     assert result ==False

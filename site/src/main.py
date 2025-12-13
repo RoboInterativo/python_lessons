16656fd48ad06ee7aa9b2bae30e80b7a51bdf521
chiefexb <chief_exb@mail.ru>
2025-12-13 17:42:39 +0300

    123

--- site/src/main.py
+++ site/src/main.py
@@ -1,10 +1,39 @@
 from flask import Flask
+from faker import Faker
+from flask import render_template
+
+# Простая инициализация (английский по умолчанию)
+
+
+# С локализацией (русские данные)
+fake = Faker('ru_RU')
+
+
+db=[]
+for i in range(100):
+    a={}
+    fullname=fake.name().split()
+    last=fullname[0]
+    first=fullname[1]
+    second= fullname[2]
+    a={"first": first,
+       "last": last,
+       "second": second
+      }
+    db.append(a)
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html', db=db)
 
 
 if __name__ == "__main__":
